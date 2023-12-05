8f3b07c0540a0c1ee3aff50a752183bb30280e11
Alex <47569754+aay92@users.noreply.github.com>
2023-12-06 00:26:38 +0400

    make sign up with UserDefaults

--- Find a friend/Application/SceneDelegate.swift
+++ Find a friend/Application/SceneDelegate.swift
@@ -10,17 +10,40 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let userDefaults = UserDefaults.standard
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+//                userDefaults.setValue(false, forKey: "isLogin")
+        //  userDefaults = false / true - для ручного выхода из приложения(Личного кабинета)
+
+        let isLogin = userDefaults.object(forKey: "isLogin") as? Bool ?? false
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-//        let tabBar = TabBar()
-        let mainViewController = AuthViewController()
-        let nav = UINavigationController(rootViewController: mainViewController)
-//        window.rootViewController = tabBar
-        window.rootViewController = nav
+
+        if isLogin {
+            startApp(window: window)
+        } else {
+            startLogin(window: window)
+        }
+    }
+    
+    private func startApp(window: UIWindow){
+        let startVC = TabBar()
+        let nav = UINavigationController(rootViewController: startVC)
         self.window = window
-        window.makeKeyAndVisible()
+        self.window?.rootViewController = nav
+        self.window?.makeKeyAndVisible()
+    }
+    
+    private func startLogin(window: UIWindow){
+        let authVC = AuthViewController()
+        let nav = UINavigationController(rootViewController: authVC)
+        self.window = window
+        self.window?.rootViewController = nav
+        self.window?.makeKeyAndVisible()
     }
 }
 
